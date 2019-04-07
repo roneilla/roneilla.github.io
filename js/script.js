853a29f6fbb6baeacf096a462a18418ce069e016
eilla <bumanlar@sheridancollege.ca>
2019-04-07 00:21:25 -0400

    pop ups done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 var bottle = document.getElementById("waste__bottle");
-
 var straw = document.getElementById("waste__straw");
-
 var yogurtCup = document.getElementById("waste__yogurtCup");
-yogurtCup
 
 var bowl = document.getElementById("creation__bowl");
 var cup = document.getElementById("creation__cup");
@@ -11,13 +8,10 @@ var clock = document.getElementById("creation__clock");
 
 var wasteCounter = 0;
 
-var bowlChosen = false;
-var cupChosen = false;
-var clockChosen = false;
-
 var createButton = document.getElementById("button");
-
 var cupPop = document.getElementById("popUp__cup");
+var bowlPop = document.getElementById("popUp__bowl");
+var clockPop = document.getElementById("popUp__clock");
 
 var exitButton = document.getElementById("popUp__exit");
 
@@ -32,13 +26,21 @@ function wasteAdded() {
 
         bowl.addEventListener('click', function bowlClicked() {
             bowl.style.backgroundColor = "#3F98D2";
-
             clock.style.filter = "saturate(0)"
             clock.style.cursor = "no-drop";
             cup.style.filter = "saturate(0)"
             cup.style.cursor = "no-drop";
             clock.style.backgroundColor = "white";
             cup.style.backgroundColor = "white";
+
+            createButton.addEventListener('click', function popUp() {
+                bowlPop.style.visibility = "visible";
+
+                exitButton.addEventListener('click', function exitPopUp() {
+                    bowlPop.style.visibility = "hidden";
+
+                })
+            })
         })
 
 
@@ -61,7 +63,6 @@ function wasteAdded() {
                 exitButton.addEventListener('click', function exitPopUp() {
                     cupPop.style.visibility = "hidden";
 
-                    // page reload
                 })
             })
 
@@ -81,6 +82,15 @@ function wasteAdded() {
                 bowl.style.backgroundColor = "white";
             "no-drop";
             cup.style.backgroundColor = "white";
+
+            createButton.addEventListener('click', function popUp() {
+                clockPop.style.visibility = "visible";
+
+                exitButton.addEventListener('click', function exitPopUp() {
+                    clockPop.style.visibility = "hidden";
+
+                })
+            })
 
         })
     }

0f97f185d52166a9e8a35f56c8fff20598fd3b84
eilla <bumanlar@sheridancollege.ca>
2019-04-09 18:56:55 -0400

    new site test
    testing vdes website

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var learnMore = document.getElementsByClassName("header__button")[0];
+
+learnMore.addEventListener('click', function learn() {
+
+    //change elements visibility to visible
+
+    //scroll down the page,, change user's Y position
+
+})

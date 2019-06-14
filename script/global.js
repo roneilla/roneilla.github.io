5fcbc8cf0ca3fc87c377ec6911d87c786f3b5f63
eilla <bumanlar@sheridancollege.ca>
2019-06-14 09:57:05 -0400

    added position and rotation customizations in arhelper code

--- script/global.js
+++ script/global.js
@@ -17,28 +17,15 @@
   // declare video variables here
   var coral = new markerVideo('videos/CORAL.mp4', 1, 1);
   coral.setSize(236, 214);
+  coral.setRotation(-Math.PI / 2, 0, 0);
+  coral.setPosition(0, 0, 0);
   coral.load();
-
-  var fish = new markerVideo('videos/FISH.mp4', 1, 1);
-  fish.setSize(402, 418);
-  fish.load();
-
-  var crab = new markerVideo('videos/CRAB.mp4', 1, 1);
-  crab.setSize(96, 96);
-  crab.load();
-
-  var turtle = new markerVideo('videos/TURTLE.mp4', 1, 1);
-  turtle.setSize(278, 278);
-  turtle.load();
 
   var shark = new markerVideo('videos/SHARK.mp4', 1, 1);
   shark.setSize(134, 136);
+  shark.setRotation(-Math.PI / 2, 0, 0);
+  shark.setPosition(0, 0, 0);
   shark.load();
-
-  var fullVideo = new markerVideo('videos/FULL.mp4', 1, 1);
-  fullVideo.setSize(1080, 1080);
-  fullVideo.load();
-
 
   // image variables
 

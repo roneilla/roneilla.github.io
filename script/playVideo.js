c16689a2be2056be9adacbd4d228b9badd71e4d3
eilla <bumanlar@sheridancollege.ca>
2019-06-14 12:24:18 -0400

    try again rip

--- script/playVideo.js
+++ script/playVideo.js
@@ -1,13 +1,16 @@
    function playVideo() {
        if (markerA.root.visible === true) {
+           event.preventDefault()
            aVideo.video.play();
            bVideo.video.pause();
            dVideo.video.pause();
        } else if (markerB.root.visible === true) {
+           event.preventDefault()
            bVideo.video.play();
            aVideo.video.pause();
            dVideo.video.pause();
        } else if (markerD.root.visible === true) {
+           event.preventDefault()
            dVideo.video.play();
            aVideo.video.pause();
            bVideo.video.pause();

5260290eee5b7d9207e878e1671b4b05809304e9
eilla <bumanlar@sheridancollege.ca>
2019-06-14 12:00:24 -0400

    Version 0.1.6

--- script/global.js
+++ script/global.js
@@ -17,19 +17,19 @@
   var marker1, marker2, marker3;
 
   // declare video variables here
-  var aVideo = new markerVideo('videos/a-video.mp4', 14.11, 5.08);
+  var aVideo = new markerVideo('videos/a-video.mp4', 7.055, 2.54);
   aVideo.setSize(2016, 726);
   aVideo.setRotation(-90 * Math.PI / 180, 0, 45 * Math.PI / 180);
   aVideo.setPosition(0, 0, 0);
   aVideo.load();
 
-  var bVideo = new markerVideo('videos/b-video.mp4', 7.56, 7.56);
+  var bVideo = new markerVideo('videos/b-video.mp4', 3.78, 3.78);
   bVideo.setSize(1080, 1080);
   bVideo.setRotation(-90 * Math.PI / 180, 0, 45 * Math.PI / 180);
   bVideo.setPosition(0, 0, 0);
   bVideo.load();
 
-  var dVideo = new markerVideo('videos/d-video.mp4', 10.50, 6.79);
+  var dVideo = new markerVideo('videos/d-video.mp4', 5.25, 3.395);
   dVideo.setSize(1500, 970);
   dVideo.setRotation(-90 * Math.PI / 180, 0, 45 * Math.PI / 180);
   dVideo.setPosition(0, 0, 0);

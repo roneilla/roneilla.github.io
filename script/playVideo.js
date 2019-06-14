b8592b8154bacdb6f51328f2fdfb5454eaee71e8
eilla <bumanlar@sheridancollege.ca>
2019-06-14 12:18:37 -0400

    pls be the last one ugh

--- script/playVideo.js
+++ script/playVideo.js
@@ -1,9 +1,15 @@
    function playVideo() {
        if (markerA.root.visible === true) {
            aVideo.video.play();
+           bVideo.video.pause();
+           dVideo.video.pause();
        } else if (markerB.root.visible === true) {
            bVideo.video.play();
+           aVideo.video.pause();
+           dVideo.video.pause();
        } else if (markerD.root.visible === true) {
            dVideo.video.play();
+           aVideo.video.pause();
+           bVideo.video.pause();
        }
    }

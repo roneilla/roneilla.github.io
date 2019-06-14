4b9148a21c7ae5f4486e43d97eadb34c7db831e2
eilla <bumanlar@sheridancollege.ca>
2019-06-14 12:43:28 -0400

    oops

--- script/global.js
+++ script/global.js
@@ -19,18 +19,18 @@
   // declare video variables here
   var aVideo = new markerVideo('videos/a-video.mp4', 7.055, 2.54);
   aVideo.setSize(2016, 726);
-  aVideo.setRotation(0, 0, 45 * Math.PI / 180);
+  aVideo.setRotation(-90 * Math.PI / 180, 0, 45 * Math.PI / 180);
   aVideo.setPosition(0, 0, 0);
   aVideo.load();
 
   var bVideo = new markerVideo('videos/b-video.mp4', 3.78, 3.78);
   bVideo.setSize(1080, 1080);
-  bVideo.setRotation(0, 0, 45 * Math.PI / 180);
+  bVideo.setRotation(-90 * Math.PI / 180, 0, 45 * Math.PI / 180);
   bVideo.setPosition(0, 0, 0);
   bVideo.load();
 
   var dVideo = new markerVideo('videos/d-video.mp4', 5.25, 3.395);
   dVideo.setSize(1500, 970);
-  dVideo.setRotation(0, 0, 45 * Math.PI / 180);
+  dVideo.setRotation(-90 * Math.PI / 180, 0, 45 * Math.PI / 180);
   dVideo.setPosition(0, 0, 0);
   dVideo.load();
